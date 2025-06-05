9c0abdcb0b5d5da56383b7b2b521a98b1b403a21
UCastellano <prof.ucastellanop@uml.edu.ni>
2025-06-05 08:54:16 -0600

    Configurar Clever Cloud Cellar para almacenamiento de PDFs

--- config/s3.js
+++ config/s3.js
@@ -2,11 +2,12 @@ const AWS = require('aws-sdk');
 require('dotenv').config();
 
 const s3 = new AWS.S3({
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  region: process.env.AWS_REGION
+  accessKeyId: process.env.CELLAR_ADDON_KEY_ID,
+  secretAccessKey: process.env.CELLAR_ADDON_KEY_SECRET,
+  endpoint: process.env.CELLAR_ADDON_HOST,
+  s3ForcePathStyle: true // Necesario para S3-compatible como Cellar
 });
 
-const S3_BUCKET_NAME = process.env.S3_BUCKET_NAME;
+const S3_BUCKET_NAME = process.env.CELLAR_BUCKET;
 
 module.exports = { s3, S3_BUCKET_NAME }; 
